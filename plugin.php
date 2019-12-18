7cc91f9b9da15ac70c3a6f3dbdb3e08746fc1a9a
Elaine Shannon <eshannon3@stmarytx.edu>
2019-12-18 14:49:54 -0600

    Step 08 - Server-side rendering

--- plugin.php
+++ plugin.php
@@ -51,6 +51,33 @@ function my_register_related_block() {
 }
  
 function get_related_posts($attributes) {
-    return '<div>Front end view will be displayed here.</div>';
+    $output = '<div class="related-block"><h2>You might also like...</h2>';
+    // Posts
+    if($attributes[postType] == 'posts') {
+        $output .= '<div class="related-posts">';
+        foreach($attributes[postIds] as $postId) {
+            $postObject = get_post($postId);
+            $output .= '<div class="related-post"><a href="' . get_the_permalink($postObject) . '">';
+            if(has_post_thumbnail($postObject)) {
+                $thumbId = get_post_thumbnail_id($postObject);
+                $altText = get_post_meta($thumbId, '_wp_attachment_image_alt', true);
+                $output .= '<img src="' . get_the_post_thumbnail_url($postObject, array(200,200)) . '" alt="' . $altText . '" />';
+            } else {
+                $output .= '<img src="https://via.placeholder.com/200" alt="No image found" />';
+            }
+            $output .= '<span>' . $postObject->post_title . '</span></a></div>';
+        }
+        $output .= '</div>';
+    // Pages
+    } elseif($attributes[postType] == 'pages') {
+        $output .= '<ul>';
+        foreach($attributes[postIds] as $postId) {
+            $postObject = get_post($postId);
+            $output .= '<li><a href="' . get_the_permalink($postObject) . '">' . $postObject->post_title . '</a></li>';
+        }
+        $output .= '</ul>';
+    }
+    // Always return output
+    return $output;
 }
 ?>
